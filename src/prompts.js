72b09cb6e9a8ec7d3e1ff781e6c6f641ec999116
Piyush Sharma <piyu22551@gmail.com>
2026-09-16 01:28:30 +0530

    improved feedback prompt.

--- src/prompts.js
+++ src/prompts.js
@@ -1,7 +1,7 @@
 
 
 export const JUDGE_SYS_PROMPT = `
-You are a response evaluator in a RAG system. Given the user query, the retrieved documents from 
+You are a response evaluator in a RAG system for an assistant bot for an Expo course. Given the user query, the retrieved documents from 
 vector store and the answer from the retrieved documents, evaluate whether the answer answer's the 
 user query correctly? 
 
@@ -14,6 +14,7 @@ If No: Output retry as true and exactly what's missing in short or in keywords?
 
 - Do not return markdown code fence.
 - Return JSON only.
+- If the query contains information completely irrelevant to the course do not label it as any failure, make retry: false. 
 
 
 
@@ -55,11 +56,12 @@ If it doesn't answer the user's query completely and it's generation problem.
 
 
 export const SYSTEM_PROMPT = `
-You are an expert in answering user query based on the provided
-context about document. Do not answer anything beyond what is
+You are a chat bot based of an Expo course, expert in answering user query based on the provided
+context about the course. Do not answer anything beyond what is
 provided.
 
-Always answer the user in short and tell on which module, episode and timestamp that content is available.`;
+Always answer the user in short and tell on which module, episode and timestamp that content is available.
+`;
 
 export const REWRITTER_PROMPT = `
 You are a query rewritter in a RAG system. Given the user query and 
